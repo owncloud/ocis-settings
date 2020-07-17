ffcec553854cf98ce7412b767e7d1cd830bdccba
Benedikt Kulmann <benedikt@kulmann.biz>
2020-07-17 17:01:34 +0200

    Regenerate js client

--- ui/client/settings/index.js
+++ ui/client/settings/index.js
@@ -29,6 +29,144 @@ export const request = (method, url, body, queryParameters, form, config) => {
  ==========================================================*/
 /**
  * 
+ * request: RoleService_AssignRoleToUser
+ * url: RoleService_AssignRoleToUserURL
+ * method: RoleService_AssignRoleToUser_TYPE
+ * raw_url: RoleService_AssignRoleToUser_RAW_URL
+ * @param body - 
+ */
+export const RoleService_AssignRoleToUser = function(parameters = {}) {
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  const config = parameters.$config
+  let path = '/api/v0/settings/assignments-add'
+  let body
+  let queryParameters = {}
+  let form = {}
+  if (parameters['body'] !== undefined) {
+    body = parameters['body']
+  }
+  if (parameters['body'] === undefined) {
+    return Promise.reject(new Error('Missing required  parameter: body'))
+  }
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    });
+  }
+  return request('post', domain + path, body, queryParameters, form, config)
+}
+export const RoleService_AssignRoleToUser_RAW_URL = function() {
+  return '/api/v0/settings/assignments-add'
+}
+export const RoleService_AssignRoleToUser_TYPE = function() {
+  return 'post'
+}
+export const RoleService_AssignRoleToUserURL = function(parameters = {}) {
+  let queryParameters = {}
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  let path = '/api/v0/settings/assignments-add'
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    })
+  }
+  let keys = Object.keys(queryParameters)
+  return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '')
+}
+/**
+ * 
+ * request: RoleService_ListRoleAssignments
+ * url: RoleService_ListRoleAssignmentsURL
+ * method: RoleService_ListRoleAssignments_TYPE
+ * raw_url: RoleService_ListRoleAssignments_RAW_URL
+ * @param body - 
+ */
+export const RoleService_ListRoleAssignments = function(parameters = {}) {
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  const config = parameters.$config
+  let path = '/api/v0/settings/assignments-list'
+  let body
+  let queryParameters = {}
+  let form = {}
+  if (parameters['body'] !== undefined) {
+    body = parameters['body']
+  }
+  if (parameters['body'] === undefined) {
+    return Promise.reject(new Error('Missing required  parameter: body'))
+  }
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    });
+  }
+  return request('post', domain + path, body, queryParameters, form, config)
+}
+export const RoleService_ListRoleAssignments_RAW_URL = function() {
+  return '/api/v0/settings/assignments-list'
+}
+export const RoleService_ListRoleAssignments_TYPE = function() {
+  return 'post'
+}
+export const RoleService_ListRoleAssignmentsURL = function(parameters = {}) {
+  let queryParameters = {}
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  let path = '/api/v0/settings/assignments-list'
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    })
+  }
+  let keys = Object.keys(queryParameters)
+  return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '')
+}
+/**
+ * 
+ * request: RoleService_RemoveRoleFromUser
+ * url: RoleService_RemoveRoleFromUserURL
+ * method: RoleService_RemoveRoleFromUser_TYPE
+ * raw_url: RoleService_RemoveRoleFromUser_RAW_URL
+ * @param body - 
+ */
+export const RoleService_RemoveRoleFromUser = function(parameters = {}) {
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  const config = parameters.$config
+  let path = '/api/v0/settings/assignments-remove'
+  let body
+  let queryParameters = {}
+  let form = {}
+  if (parameters['body'] !== undefined) {
+    body = parameters['body']
+  }
+  if (parameters['body'] === undefined) {
+    return Promise.reject(new Error('Missing required  parameter: body'))
+  }
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    });
+  }
+  return request('post', domain + path, body, queryParameters, form, config)
+}
+export const RoleService_RemoveRoleFromUser_RAW_URL = function() {
+  return '/api/v0/settings/assignments-remove'
+}
+export const RoleService_RemoveRoleFromUser_TYPE = function() {
+  return 'post'
+}
+export const RoleService_RemoveRoleFromUserURL = function(parameters = {}) {
+  let queryParameters = {}
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  let path = '/api/v0/settings/assignments-remove'
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    })
+  }
+  let keys = Object.keys(queryParameters)
+  return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '')
+}
+/**
+ * 
  * request: BundleService_GetSettingsBundle
  * url: BundleService_GetSettingsBundleURL
  * method: BundleService_GetSettingsBundle_TYPE
@@ -121,6 +259,52 @@ export const BundleService_SaveSettingsBundleURL = function(parameters = {}) {
 }
 /**
  * 
+ * request: BundleService_AddSettingToSettingsBundle
+ * url: BundleService_AddSettingToSettingsBundleURL
+ * method: BundleService_AddSettingToSettingsBundle_TYPE
+ * raw_url: BundleService_AddSettingToSettingsBundle_RAW_URL
+ * @param body - 
+ */
+export const BundleService_AddSettingToSettingsBundle = function(parameters = {}) {
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  const config = parameters.$config
+  let path = '/api/v0/settings/bundles-add-setting'
+  let body
+  let queryParameters = {}
+  let form = {}
+  if (parameters['body'] !== undefined) {
+    body = parameters['body']
+  }
+  if (parameters['body'] === undefined) {
+    return Promise.reject(new Error('Missing required  parameter: body'))
+  }
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    });
+  }
+  return request('post', domain + path, body, queryParameters, form, config)
+}
+export const BundleService_AddSettingToSettingsBundle_RAW_URL = function() {
+  return '/api/v0/settings/bundles-add-setting'
+}
+export const BundleService_AddSettingToSettingsBundle_TYPE = function() {
+  return 'post'
+}
+export const BundleService_AddSettingToSettingsBundleURL = function(parameters = {}) {
+  let queryParameters = {}
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  let path = '/api/v0/settings/bundles-add-setting'
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    })
+  }
+  let keys = Object.keys(queryParameters)
+  return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '')
+}
+/**
+ * 
  * request: BundleService_ListSettingsBundles
  * url: BundleService_ListSettingsBundlesURL
  * method: BundleService_ListSettingsBundles_TYPE
@@ -157,6 +341,98 @@ export const BundleService_ListSettingsBundlesURL = function(parameters = {}) {
   let queryParameters = {}
   const domain = parameters.$domain ? parameters.$domain : getDomain()
   let path = '/api/v0/settings/bundles-list'
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    })
+  }
+  let keys = Object.keys(queryParameters)
+  return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '')
+}
+/**
+ * 
+ * request: BundleService_RemoveSettingFromSettingsBundle
+ * url: BundleService_RemoveSettingFromSettingsBundleURL
+ * method: BundleService_RemoveSettingFromSettingsBundle_TYPE
+ * raw_url: BundleService_RemoveSettingFromSettingsBundle_RAW_URL
+ * @param body - 
+ */
+export const BundleService_RemoveSettingFromSettingsBundle = function(parameters = {}) {
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  const config = parameters.$config
+  let path = '/api/v0/settings/bundles-remove-setting'
+  let body
+  let queryParameters = {}
+  let form = {}
+  if (parameters['body'] !== undefined) {
+    body = parameters['body']
+  }
+  if (parameters['body'] === undefined) {
+    return Promise.reject(new Error('Missing required  parameter: body'))
+  }
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    });
+  }
+  return request('post', domain + path, body, queryParameters, form, config)
+}
+export const BundleService_RemoveSettingFromSettingsBundle_RAW_URL = function() {
+  return '/api/v0/settings/bundles-remove-setting'
+}
+export const BundleService_RemoveSettingFromSettingsBundle_TYPE = function() {
+  return 'post'
+}
+export const BundleService_RemoveSettingFromSettingsBundleURL = function(parameters = {}) {
+  let queryParameters = {}
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  let path = '/api/v0/settings/bundles-remove-setting'
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    })
+  }
+  let keys = Object.keys(queryParameters)
+  return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '')
+}
+/**
+ * 
+ * request: RoleService_ListRoles
+ * url: RoleService_ListRolesURL
+ * method: RoleService_ListRoles_TYPE
+ * raw_url: RoleService_ListRoles_RAW_URL
+ * @param body - 
+ */
+export const RoleService_ListRoles = function(parameters = {}) {
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  const config = parameters.$config
+  let path = '/api/v0/settings/roles-list'
+  let body
+  let queryParameters = {}
+  let form = {}
+  if (parameters['body'] !== undefined) {
+    body = parameters['body']
+  }
+  if (parameters['body'] === undefined) {
+    return Promise.reject(new Error('Missing required  parameter: body'))
+  }
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    });
+  }
+  return request('post', domain + path, body, queryParameters, form, config)
+}
+export const RoleService_ListRoles_RAW_URL = function() {
+  return '/api/v0/settings/roles-list'
+}
+export const RoleService_ListRoles_TYPE = function() {
+  return 'post'
+}
+export const RoleService_ListRolesURL = function(parameters = {}) {
+  let queryParameters = {}
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  let path = '/api/v0/settings/roles-list'
   if (parameters.$queryParameters) {
     Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
       queryParameters[parameterName] = parameters.$queryParameters[parameterName]
