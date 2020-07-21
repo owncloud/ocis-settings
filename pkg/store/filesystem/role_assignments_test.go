4cfa0fab898e8df442d9f5e5eb61d7ba06ba72cc
A.Unger <zyxancf@gmail.com>
2020-07-21 12:24:22 +0200

    use table testing

--- pkg/store/filesystem/role_assignments_test.go
+++ pkg/store/filesystem/role_assignments_test.go
@@ -18,35 +18,43 @@ var (
 		olog.Level("info"),
 	)
 
-	bundles = []*proto.SettingsBundle{
+	scenarios = []struct {
+		Bundle      *proto.SettingsBundle
+		AccountUUID string
+	}{
 		{
-			Type:        proto.SettingsBundle_TYPE_ROLE,
-			DisplayName: "test role - reads | update",
-			Name:        "TEST_ROLE",
-			Extension:   "ocis-settings",
-			Resource: &proto.Resource{
-				Type: proto.Resource_TYPE_BUNDLE,
-			},
-			Settings: []*proto.Setting{
-				{
-					Name: "update",
-					Value: &proto.Setting_PermissionValue{
-						PermissionValue: &proto.PermissionSetting{
-							Operation: proto.PermissionSetting_OPERATION_UPDATE,
+			Bundle: &proto.SettingsBundle{
+				Type:        proto.SettingsBundle_TYPE_ROLE,
+				DisplayName: "test role - reads | update",
+				Name:        "TEST_ROLE",
+				Extension:   "ocis-settings",
+				Resource: &proto.Resource{
+					Type: proto.Resource_TYPE_BUNDLE,
+				},
+				Settings: []*proto.Setting{
+					{
+						Name: "update",
+						Value: &proto.Setting_PermissionValue{
+							PermissionValue: &proto.PermissionSetting{
+								Operation: proto.PermissionSetting_OPERATION_UPDATE,
+							},
+						},
+					},
+					{
+						Name: "read",
+						Value: &proto.Setting_PermissionValue{
+							PermissionValue: &proto.PermissionSetting{
+								Operation: proto.PermissionSetting_OPERATION_READ,
+							},
 						},
 					},
 				},
-				{
-					Name: "read",
-					Value: &proto.Setting_PermissionValue{
-						PermissionValue: &proto.PermissionSetting{
-							Operation: proto.PermissionSetting_OPERATION_READ,
-						},
-					},
-				},
 			},
+			AccountUUID: "a4d07560-a670-4be9-8d60-9b547751a208",
 		},
 	}
+	// TODO: add red tests based on specs. For instance, should be having 2 role assignments to the same accountID
+	// pointing to the same role be allowed?
 )
 
 func TestRoleAssignments(t *testing.T) {
@@ -58,18 +66,20 @@ func TestRoleAssignments(t *testing.T) {
 		Logger:   logger,
 	}
 
-	res, err := s.WriteBundle(bundles[0])
-	if err != nil {
-		t.Error(err)
-	}
+	for i := range scenarios {
+		res, err := s.WriteBundle(scenarios[i].Bundle)
+		if err != nil {
+			t.Error(err)
+		}
 
-	roleAssignment, err := s.WriteRoleAssignment(&proto.UserRoleAssignment{
-		AccountUuid: "a4d07560-a670-4be9-8d60-9b547751a208",
-		RoleId:      res.Id,
-	})
-	if err != nil {
-		t.Error(err)
-	}
+		roleAssignment, err := s.WriteRoleAssignment(&proto.UserRoleAssignment{
+			AccountUuid: scenarios[i].AccountUUID,
+			RoleId:      res.Id,
+		})
+		if err != nil {
+			t.Error(err)
+		}
 
-	assert.FileExists(t, filepath.Join(dataRoot, "role-assignments", roleAssignment.Id+".json"))
+		assert.FileExists(t, filepath.Join(dataRoot, "role-assignments", roleAssignment.Id+".json"))
+	}
 }
