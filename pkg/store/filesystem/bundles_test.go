147b06d5b6dee5228300ded4e137263fa90339c2
Benedikt Kulmann <benedikt@kulmann.biz>
2020-07-22 12:49:57 +0200

    Add test for checking that ListBundles only returns correct types

--- pkg/store/filesystem/bundles_test.go
+++ pkg/store/filesystem/bundles_test.go
@@ -9,81 +9,99 @@ import (
 )
 
 var bundleScenarios = []struct {
-	name string
-	in   struct {
-		record   *proto.SettingsBundle
-	}
-	out interface{}
+	name   string
+	bundle *proto.SettingsBundle
 }{
 	{
 		name: "generic-test-file-resource",
-		in: struct {
-			record   *proto.SettingsBundle
-		}{
-			record: &proto.SettingsBundle{
-				Id:          bundle1,
-				Extension:   extension1,
-				DisplayName: "test1",
-				Resource: &proto.Resource{
-					Type: proto.Resource_TYPE_FILE,
-					Id:   "beep",
-				},
-				Settings: []*proto.Setting{
-					{
-						Id:          setting1,
-						Description: "test-desc-1",
-						DisplayName: "test-displayname-1",
-						Resource: &proto.Resource{
-							Type: proto.Resource_TYPE_FILE,
-							Id:   "bleep",
-						},
-						Value: &proto.Setting_IntValue{
-							IntValue: &proto.IntSetting{
-								Min: 0,
-								Max: 42,
-							},
+		bundle: &proto.SettingsBundle{
+			Id:          bundle1,
+			Type:        proto.SettingsBundle_TYPE_DEFAULT,
+			Extension:   extension1,
+			DisplayName: "test1",
+			Resource: &proto.Resource{
+				Type: proto.Resource_TYPE_FILE,
+				Id:   "beep",
+			},
+			Settings: []*proto.Setting{
+				{
+					Id:          setting1,
+					Description: "test-desc-1",
+					DisplayName: "test-displayname-1",
+					Resource: &proto.Resource{
+						Type: proto.Resource_TYPE_FILE,
+						Id:   "bleep",
+					},
+					Value: &proto.Setting_IntValue{
+						IntValue: &proto.IntSetting{
+							Min: 0,
+							Max: 42,
 						},
 					},
 				},
 			},
 		},
-		out: nil,
 	},
 	{
 		name: "generic-test-system-resource",
-		in: struct {
-			record   *proto.SettingsBundle
-		}{
-			record: &proto.SettingsBundle{
-				Id:          bundle2,
-				Extension:   extension2,
-				DisplayName: "test1",
-				Resource: &proto.Resource{
-					Type: proto.Resource_TYPE_SYSTEM,
-				},
-				Settings: []*proto.Setting{
-					{
-						Id:          setting2,
-						Description: "test-desc-2",
-						DisplayName: "test-displayname-2",
-						Resource: &proto.Resource{
-							Type: proto.Resource_TYPE_SYSTEM,
-						},
-						Value: &proto.Setting_IntValue{
-							IntValue: &proto.IntSetting{
-								Min: 0,
-								Max: 42,
-							},
+		bundle: &proto.SettingsBundle{
+			Id:          bundle2,
+			Type:        proto.SettingsBundle_TYPE_DEFAULT,
+			Extension:   extension2,
+			DisplayName: "test1",
+			Resource: &proto.Resource{
+				Type: proto.Resource_TYPE_SYSTEM,
+			},
+			Settings: []*proto.Setting{
+				{
+					Id:          setting2,
+					Description: "test-desc-2",
+					DisplayName: "test-displayname-2",
+					Resource: &proto.Resource{
+						Type: proto.Resource_TYPE_SYSTEM,
+					},
+					Value: &proto.Setting_IntValue{
+						IntValue: &proto.IntSetting{
+							Min: 0,
+							Max: 42,
 						},
 					},
 				},
 			},
 		},
-		out: nil,
+	},
+	{
+		name: "generic-test-role-bundle",
+		bundle: &proto.SettingsBundle{
+			Id:          bundle3,
+			Type:        proto.SettingsBundle_TYPE_ROLE,
+			Extension:   extension1,
+			DisplayName: "Role1",
+			Resource: &proto.Resource{
+				Type: proto.Resource_TYPE_SYSTEM,
+			},
+			Settings: []*proto.Setting{
+				{
+					Id:          setting3,
+					Description: "test-desc-3",
+					DisplayName: "test-displayname-3",
+					Resource: &proto.Resource{
+						Type: proto.Resource_TYPE_SETTING,
+						Id:   setting1,
+					},
+					Value: &proto.Setting_PermissionValue{
+						PermissionValue: &proto.PermissionSetting{
+							Operation:  proto.PermissionSetting_OPERATION_READ,
+							Constraint: proto.PermissionSetting_CONSTRAINT_OWN,
+						},
+					},
+				},
+			},
+		},
 	},
 }
 
-func TestWriteSettingsBundleToFile(t *testing.T) {
+func TestSettingsBundles(t *testing.T) {
 	s := Store{
 		dataPath: dataRoot,
 		Logger: olog.NewLogger(
@@ -92,15 +110,35 @@ func TestWriteSettingsBundleToFile(t *testing.T) {
 			olog.Level("info"),
 		),
 	}
+
+	// write bundles
 	for i := range bundleScenarios {
 		index := i
 		t.Run(bundleScenarios[index].name, func(t *testing.T) {
-			filePath := s.buildFilePathForBundle(bundleScenarios[index].in.record.Id, true)
-			if err := s.writeRecordToFile(bundleScenarios[index].in.record, filePath); err != nil {
+			filePath := s.buildFilePathForBundle(bundleScenarios[index].bundle.Id, true)
+			if err := s.writeRecordToFile(bundleScenarios[index].bundle, filePath); err != nil {
 				t.Error(err)
 			}
 			assert.FileExists(t, filePath)
 		})
+	}
+
+	// check that ListBundles only returns bundles with type DEFAULT
+	bundles, err := s.ListBundles(proto.SettingsBundle_TYPE_DEFAULT)
+	if err != nil {
+		t.Error(err)
+	}
+	for i := range bundles {
+		assert.Equal(t, proto.SettingsBundle_TYPE_DEFAULT, bundles[i].Type)
+	}
+
+	// check that ListRoles only returns bundles with type ROLE
+	roles, err := s.ListBundles(proto.SettingsBundle_TYPE_ROLE)
+	if err != nil {
+		t.Error(err)
+	}
+	for i := range roles {
+		assert.Equal(t, proto.SettingsBundle_TYPE_ROLE, roles[i].Type)
 	}
 
 	burnRoot()
